ba873f6a7f056187e461265b45311d6ac8505b14
Daniel Guzman <daniel@projectcor.com>
2019-08-07 12:03:41 -0300

    Handling the instruction after install

--- instructions.js
+++ instructions.js
@@ -2,17 +2,11 @@
 
 const path = require('path')
 
-async function makeConfigFile (cli) {
-    try {
-      const inFile = path.join(__dirname, './config', 'index.js')
-      const outFile = path.join(cli.helpers.configPath(), 'cor-sdk.js')
-      await cli.copy(inFile, outFile)
-      cli.command.completed('create', 'config/cor-sdk.js')
-    } catch (error) {
-      // ignore error
-    }
-}
-
-module.exports = {
-    makeConfigFile(cli)
+module.exports = async (cli) => {
+  try {
+    await cli.copy(path.join(__dirname, 'config/index.js'), path.join(cli.helpers.configPath(), 'cor-sdk.js'))
+    cli.command.completed('create', 'config/cor-sdk.js')
+  } catch (error) {
+    console.log(error)
+  }
 }
